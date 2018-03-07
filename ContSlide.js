e3d45b23288369a88b34b1a8f43ef5d7907d3fe1
Jay Dickson <jaydickson02@gmail.com>
2018-03-07 21:44:21 +1100

    Design Additions

--- ContSlide.js
+++ ContSlide.js
@@ -1,14 +1,19 @@
 
 
-var foo = array('placeholder1', 'placeholder2');
-document.getElementById("fooHolder").innerHTML = foo.toString();
+content = ["So to start, i'm a first year IT student at RMIT. I enjoy solving puzzles and problems. Whether it be a rubiks cube or a complex math problem, i'm always willing to give it a go. Even if failure is more than likely (six hours to solve my first rubiks cube).", "Hello"];
+x = 0
 
 function Next(){
-
+document.getElementById("About").innerHTML = content[x];
+x += 1
 
 }
 
 function Back(){
+document.getElementById("About").innerHTML = content[x];
+x -= 1
+if(x == -1){
+  x = 1;
+}
 
-  
 }
